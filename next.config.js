044ece3fb5cce155fcb1db5146c5fdec1c0c4fe8
zebulon <milandala89@gmail.com>
2021-06-20 15:42:57 +0200

    cnfig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
+module.exports = {
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en", "hu", "de"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "hu",
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import customTheme from "../theme";
+import { I18nProvider } from "next-localization";
+
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const { lngDict, ...rest } = pageProps;
+
+  return (
+    <>
+      <Head>
+        <title>Anzenta</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <I18nProvider lngDict={lngDict} locale={router.locale}>
+        <ChakraProvider theme={customTheme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </I18nProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
